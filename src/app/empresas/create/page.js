16a2594f44c71303ba7090b2658e62804f66ad9c
MatheusBytes <matheusr.monte2004@gmail.com>
2024-09-26 20:24:43 -0300

    localstorage

--- src/app/empresas/create/page.js
+++ src/app/empresas/create/page.js
@@ -1,65 +1,55 @@
 "use client";
 
 import Pagina from "@/app/components/Pagina";
-import { Form,Button } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import { FaCheck } from "react-icons/fa";
-import Link from "next/link"
+import Link from "next/link";
 import { IoArrowBack } from "react-icons/io5";
-import { Formik } from 'formik';
+import { Formik } from "formik";
 
 export default function Page() {
-
-
-  function salvar(dados){
-    console.log(dados);
+  function salvar(dados) {
+    const empresas = [];
+    empresas.push(dados);
+    localStorage.setItem("empresas", JSON.stringify(empresas));
   }
+
   return (
     <Pagina titulo="Formulário">
-
       <Formik
-        initialValues={{ nome: '', logo: '' }}
-        onSubmit={values => salvar(values)}
+        initialValues={{ nome: "", logo: "" }}
+        onSubmit={(values) => salvar(values)}
       >
-      {({
-         values,
-         handleChange,
-         handleSubmit,
-       
-       }) => (
-      <Form>
-        <Form.Group className="mb-3" controlId="nome">
-          <Form.Label>Nome</Form.Label>
-          <Form.Control 
-          type="text" 
-          name="nome" 
-          value={values.nome}
-          onChange={handleChange('nome')}
-          />
-
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="logo">
-          <Form.Label>logo</Form.Label>
-          <Form.Control 
-          type="text" 
-          name="logo"
-          value={values.logo}
-          onChange={handleChange('logo')}
-          />
-        </Form.Group>
-       <div className='text-center'>
-         
-        <Button onClick={handleSubmit} variant='success'> 
-        <FaCheck /> Salvar
-        </Button>
-        <Link 
-                href="/empresas" 
-                className="btn btn-warning ms-2"
-            >
-               <IoArrowBack /> voltar
-            </Link>
-       </div>
-      </Form>
-       )}
+        {({ values, handleChange, handleSubmit }) => (
+          <Form>
+            <Form.Group className="mb-3" controlId="nome">
+              <Form.Label>Nome</Form.Label>
+              <Form.Control
+                type="text"
+                name="nome"
+                value={values.nome}
+                onChange={handleChange("nome")}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="logo">
+              <Form.Label>logo</Form.Label>
+              <Form.Control
+                type="text"
+                name="logo"
+                value={values.logo}
+                onChange={handleChange("logo")}
+              />
+            </Form.Group>
+            <div className="text-center">
+              <Button onClick={handleSubmit} variant="success">
+                <FaCheck /> Salvar
+              </Button>
+              <Link href="/empresas" className="btn btn-warning ms-2">
+                <IoArrowBack /> voltar
+              </Link>
+            </div>
+          </Form>
+        )}
       </Formik>
     </Pagina>
   );
--- src/app/empresas/page.js
+++ src/app/empresas/page.js
@@ -1,48 +1,38 @@
-'use client'
+"use client";
 
-
-import Link from "next/link"
-import { Table } from "react-bootstrap"
+import Link from "next/link";
+import { Table } from "react-bootstrap";
 import { FaPlusCircle } from "react-icons/fa";
 import Pagina from "../components/Pagina";
 
 export default function Page() {
-    return (
-        <Pagina titulo="Empresas">
+  const empresas = JSON.parse(localStorage.getItem("empresas"));
 
-            <Link 
-                href="/empresas/create" 
-                className="btn btn-primary mb-3 mt-3"
-            >
-                <FaPlusCircle /> Novo
-            </Link>
+  return (
+    <Pagina titulo="Empresas">
+      <Link href="/empresas/create" className="btn btn-primary mb-3 mt-3">
+        <FaPlusCircle /> Novo
+      </Link>
 
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Companhias</th>
-                        <th>Logo</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Gol</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Latam</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Azul</td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </Table>
-        </Pagina>
-    )
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Companhias</th>
+            <th>Logo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {empresas.map((item) => (
+            <tr>
+              <td>1</td>
+              <td>{item.nome}</td>
+              <td>{item.logo}</td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Pagina>
+  );
 }
