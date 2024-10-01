3c1d3359caeeea4c9e83e6788cc6528bcde9611b
MatheusBytes <matheusr.monte2004@gmail.com>
2024-09-30 22:02:08 -0300

    re

--- src/app/empresas/page.js
+++ src/app/empresas/page.js
@@ -10,10 +10,13 @@ import { FaRegEdit } from "react-icons/fa";
 export default function Page() {
   const empresas = JSON.parse(localStorage.getItem("empresas")) || [];
 
-  function excluir(id){
-    if(confirm(''))
-  }
-
+  function excluir(id) {
+    if (confirm('Deseja realmente excluir o registro?')) {
+        const dados = empresas.filter(item => item.id != id)
+        localStorage.setItem('empresas', JSON.stringify(dados))
+        setEmpresas(dados)
+    }
+}
   return (
     <Pagina titulo="Empresas">
       <Link href="/empresas/create" className="btn btn-primary mb-3 mt-3">
